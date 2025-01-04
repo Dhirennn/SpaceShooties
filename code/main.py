d0424d714ed3cd6b0a21bc3fefe7fe1413539c8f
Dhiren <77841136+Dhirennn@users.noreply.github.com>
2025-01-04 09:41:44 +0800

    Add meteor and laser

--- code/main.py
+++ code/main.py
@@ -21,12 +21,20 @@ pygame.display.set_caption("SpaceShooties")
 
 # Spaceship
 spaceship_surf = pygame.image.load(join('images','player.png')).convert_alpha()
-spaceship_rect = spaceship_surf.get_rect(center=(WIND0W_WIDTH/2, WINDOW_HEIGHT/2))
+spaceship_rect = spaceship_surf.get_frect(center=(WIND0W_WIDTH/2, WINDOW_HEIGHT/2))
 
 # Stars
 star_surf = pygame.image.load(join('images','star.png')).convert_alpha()
-
 star_positions = [(randint(0, WIND0W_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
+# Meteor
+meteor_surf = pygame.image.load(join('images','meteor.png')).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center=(WIND0W_WIDTH/2, WINDOW_HEIGHT/2))
+
+# Laser
+laser_surf = pygame.image.load(join('images','laser.png')).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
+
 
 
 while is_game_running:
@@ -44,6 +52,15 @@ while is_game_running:
         display_surface.blit(star_surf, star_position)
 
     display_surface.blit(spaceship_surf, spaceship_rect)
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
+
+
+
+    spaceship_rect.left += 0.5
+
+    if spaceship_rect.right == WIND0W_WIDTH:
+        spaceship_rect.left = 0
 
     # Draw the game
     pygame.display.update()
